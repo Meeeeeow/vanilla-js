f79769de7933449bbbb56ddb1e7292c41e7d320e
Plunderer <kazutokun467@gmail.com>
2020-06-17 02:47:47 +0600

    Coursera

---
+++ Js_Tutorial/script1.js
@@ -0,0 +1,11 @@
+//HTTP  = hyper text transfer protocol
+//URN = uniform resource name(name of resource)
+//URI = uniform resource identifier(location of resource)
+//URL = unifor resource locator(full site name)
+//HTTP RESPONSE STRUCTURE
+//HTTP1.1(HTTP VERSION) 200(Response status code)
+// OK(response status code in english)
+//404 -Server cannot find the request
+//200 - ok
+// 403 - unauthorized entry
+//500 - internal server error
